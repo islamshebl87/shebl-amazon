e9f313c620e70969f2b79553e8b3b9678a80f2df
islamshebl <59154145+islamshebl87@users.noreply.github.com>
2021-12-26 19:01:33 +0200

    Add files via upload

---
+++ precache-manifest.eae1532c768cea8d978a5e3136949949.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fda7e234f170b12204ad27417f08f6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cd1caff5a249618cc1f",
+    "url": "/static/css/main.78c7c615.chunk.css"
+  },
+  {
+    "revision": "66057293e9aff8f36eaf",
+    "url": "/static/js/2.1152b9cd.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/static/js/2.1152b9cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cd1caff5a249618cc1f",
+    "url": "/static/js/main.df61fce6.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
